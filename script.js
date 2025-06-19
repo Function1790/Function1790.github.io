88084ac81005433c95dc4a9ae01a80167af48870
Synt4xT3rr0r(Function) <67085909+Function1790@users.noreply.github.com>
2025-06-20 02:47:55 +0900

    1차 완성본, 프로젝트, 연락처 추가

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+// Title
 const title = document.getElementsByClassName("typing")[0];
-const cursor = document.getElementsByClassName("cursor")[0];
 const texts = {
   title: "당연함에 의문을 던지고,\n익숙함 속에서 새로움을 찾습니다.",
 };
+const cursorHTML = '<span class="cursor">.</span>';
 
 const print = console.log;
 function sleep(t) {
@@ -10,30 +11,64 @@ function sleep(t) {
 }
 
 (async function () {
-  title.innerHTML = "|";
+  title.innerHTML = cursorHTML;
   let time, output, index;
   for (var i in texts.title) {
     time = 70;
     output = texts.title[i];
     index = i;
     if (texts.title[i] == "\\") {
-      time = 200;
+      time = 250;
     } else if (texts.title[i] == "\n") {
       output = "<br>";
-      time = 200;
+      time = 250;
     }
-    print(title)
-    title.innerHTML = title.innerHTML.replace("|", "");
+    title.innerHTML = title.innerHTML.replace(cursorHTML, "");
     title.innerHTML += output;
-    title.innerHTML += '|';
+    title.innerHTML += cursorHTML;
     await sleep(time);
   }
-  title.innerHTML = title.innerHTML.replace("|", "");
-})();
-
-(async function () {
-  while(true) {
-    break;
-    sleep(100);
+  //title.innerHTML = title.innerHTML.replace("|", "");
+  cursor = document.getElementsByClassName("cursor")[0];
+  while (true) {
+    cursor.style.backgroundColor = "transparent";
+    await sleep(1000);
+    cursor.style.backgroundColor = "white";
+    await sleep(1000);
   }
 })();
+
+// Fade
+const elements = document.querySelectorAll(".fade-up");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  { threshold: 0.1 }
+);
+
+elements.forEach((el) => observer.observe(el));
+
+// Projects
+const projectItems = document.querySelectorAll(".project-item");
+
+const observer2 = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer2.unobserve(entry.target); // 한 번만 실행되도록
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+projectItems.forEach((item) => observer2.observe(item));
